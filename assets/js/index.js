93d6c4c656f264da0f34a6709c81c2671a9eb60f
trickzhavia <patrick@zhavia.com.br>
2023-06-21 13:13:07 -0300

    adding faces detections

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 const cam = document.getElementById("cam");
 
-navigator.mediaDevices.enumerateDevices().then(devices => {
+const startVideo = () => {
+    navigator.mediaDevices.enumerateDevices().then(devices => {
     if (Array.isArray(devices)) {
         devices.forEach(device => {
             if (device.kind === 'videoinput') {
@@ -10,10 +11,49 @@ navigator.mediaDevices.enumerateDevices().then(devices => {
                     }},
                     stream => cam.srcObject = stream,
                     error => console.error(error)
-                )
-            }
-        })
-    }
-})
+                    )
+                }
+            })
+        }
+    })
+}
 
-// 17 min
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./assets/lib/face-api/models'), // Detecta rostos no vídeo
+    faceapi.nets.faceLandmark68Net.loadFromUri('./assets/lib/face-api/models'), // Desenha os traços do rosto
+    faceapi.nets.faceRecognitionNet.loadFromUri('./assets/lib/face-api/models'), // Reconhecimento dentro do rosto (quem sou eu) 
+    faceapi.nets.faceExpressionNet.loadFromUri('./assets/lib/face-api/models'), // Detecta expressões
+    faceapi.nets.ageGenderNet.loadFromUri('./assets/lib/face-api/models'), // Detecta idade e gênero
+    faceapi.nets.ssdMobilenetv1.loadFromUri('./assets/lib/face-api/models') // Usada internamente pra detectar o rosto
+]).then(startVideo);
+
+cam.addEventListener('play', async () => {
+    const canvas = faceapi.createCanvasFromMedia(cam)
+    
+    const canvasSize = {
+        width: cam.width,
+        height: cam.height
+    }
+
+    faceapi.matchDimensions(canvas, canvasSize)
+    document.body.appendChild(canvas);
+
+    setInterval( async () => {
+        const detections = await faceapi
+            .detectAllFaces(
+                cam, 
+                new faceapi.TinyFaceDetectorOptions()
+            )
+            .withFaceLandmarks()
+
+        const resizeDetections = faceapi.resizeResults(detections, canvasSize);
+
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+        faceapi.draw.drawDetections(canvas, resizeDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizeDetections);
+
+    }, 100)
+});
+
+// 33 min
